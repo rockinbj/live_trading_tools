baf5d21f43be1aa4deb96e080e3f6f8387aac71c
Rock <luxxsys@gmail.com>
2023-04-02 15:43:08 +0800

    add equity line and drawdown line legend text

--- my_functions.py
+++ my_functions.py
@@ -363,7 +363,7 @@ def drawPic(equityFile, posFile):
     ax2 = ax.twinx()
     ax2.set_ylim(0, -1)
     ax2.invert_yaxis()  # 回撤的y轴反转
-    ax2.plot(eqDf["saveTime"], eqDf["drawdown"], color=(0.45, 0, 0), label="回撤")
+    ax2.plot(eqDf["saveTime"], eqDf["drawdown"], color="tab:red", label="回撤")
     ax2.yaxis.set_major_formatter(FuncFormatter(lambda y, _: f'{100 * y:.0f}%'))
     ax2.set_ylabel("回撤 (距最高点的跌幅)")
 
@@ -386,7 +386,7 @@ def drawPic(equityFile, posFile):
     lines, labels = ax.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
     ax.legend(lines + lines2, labels + labels2,
-              loc='center right', facecolor='black', edgecolor='gray', labelcolor="gray",
+              loc='center right', facecolor='black', edgecolor='white', labelcolor="white",
               bbox_to_anchor=(0.96, 0.5))
 
     # 优化一些图片显示
